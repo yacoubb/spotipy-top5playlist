ef8f77fb460740d467ac4087172b4a3249ba95e4
Yacoub Ahmed <yacoub.ahmedy@gmail.com>
2018-07-08 16:58:41 +0100

    Added script and updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# spotipy-top5playlist
-A simple python script used to create spotify playlists from a text file containing artist names.
+# top5playlist.py
+
+## A lightweight script making use of the spotipy library to bulk-add a collection of artists' top songs to a playlist on spotify.
+
+* Artists are listed in a plain text file *artists.txt* .
+* \> indents are used to indicate a new playlist title. All artist names under a title are added into that playlist.
+You can specify a new playlist title at any time.
+* \# Hashtags are commented out, they aren't read by the script.
+* $ dollar signs are used to break the list apart. In the Reading Festival example they are used to seperate the different artists by the days that they perform.
+
+You will need access to a spotify app which can easily be created at https://developer.spotify.com/dashboard/ Once you create your app, add the client id and client secret into the data.json file. You will also need to set a callback redirect url, for now something we only need a simple temporary location, http://localhost:8888/callback/ will do just fine. Add this redirect url to the data.json file too.
+
+To run the script cd into the folder containing the script and run
+```python
+python top5playlist.py <username> <song_count>
+```
+where *username* is your spotify username and *song_count* is the number of top songs you want to add from each artist, up to 10.
+After you run the script you will be promted with a web page from spotify asking for confirmation that you want to use the app. Click yes and a new playlist will be created and will start filling up with songs.
---
+++ top5playlist.py
@@ -0,0 +1,80 @@
+import sys
+import json
+import spotipy
+import spotipy.util as util
+
+cid = ""
+secret = ""
+redirect = ""
+username = ""
+song_count = 5
+
+if len(sys.argv) > 2:
+    username = sys.argv[1]
+    song_count = int( sys.argv[2] )
+    if song_count <= 0:
+        print("Song count must be an integer between 1 and 10.")
+        sys.exit(0)
+    with open("data.json") as f:
+        data = json.load(f)
+        cid = data["client_id"]
+        secret = data["client_secret"]
+        redirect = data["redirect_url"]
+else:
+    print ("Usage: %s username songcount" % (sys.argv[0],))
+    sys.exit(0)
+scope = 'playlist-modify-public'
+token = util.prompt_for_user_token(username, scope, client_id=cid, client_secret=secret, redirect_uri=redirect)
+
+if not token:
+    sys.exit(0)
+
+sp = spotipy.Spotify(auth=token)
+sp.trace = False
+
+print(sp.search('artist:Lil Peep', type='artist', limit=10))
+sys.exit(0)
+artist_dict = {}
+keys = []
+section = 1
+text_doc = open("artists.txt", "r")
+for line in text_doc:
+    if(line.startswith('#')):
+        continue
+    if line.startswith('>'):
+        section = 1
+        keys.append(line[1:-1])
+        artist_dict[keys[-1]] = {}
+        artist_dict[keys[-1]][str(section)] = []
+    else:
+        if line.startswith('$'):
+            section += 1
+            artist_dict[keys[-1]][str(section)] = []
+        else:
+            artist_dict[keys[-1]][str(section)].append(line[0:-1])
+
+for key in artist_dict:
+    print("")
+    print(key)
+    sp.user_playlist_create(sp.me()['id'], key, True)
+    current_playlist_id = sp.current_user_playlists()['items'][0]['id']
+    print(current_playlist_id)
+    for day in artist_dict[key]:
+        print(day)
+        print(artist_dict[key][day])
+        for artist_name in artist_dict[key][day]:
+            print(artist_name)
+            results = sp.search('artist:' + artist_name, type='artist', limit=1)
+            if len(results['artists']['items']) == 0:
+                continue
+            artist_id = results['artists']['items'][0]['id']
+            i = 0
+            tracks = []
+            if(len(sp.artist_top_tracks(artist_id)['tracks']) == 0):
+                continue
+            for track in sp.artist_top_tracks(artist_id)['tracks']:
+                if(i < song_count):
+                    print(track['name'], track['id'])
+                    tracks.append(track['id'])
+                    i += 1
+            sp.user_playlist_add_tracks(sp.me()['id'], current_playlist_id, tracks)
